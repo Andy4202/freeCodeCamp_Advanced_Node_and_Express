1989ab858dc566df280e69bce4660e66f4f6ec12
Andy Allan <Andy4202@gmail.com>
2022-04-30 18:44:10 +0100

    Challenge 5 complete.

--- server.js
+++ server.js
@@ -29,6 +29,30 @@ app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug/index.pug', {title: 'Hello', message: 'Please login'});
 });
 
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+  // Serialization and deserialization here...
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
+});
+// app.listen out here...
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
@@ -40,7 +64,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  // });
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, doc);
+   });
 });
