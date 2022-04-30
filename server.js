b2cecb1bc1633f5b52bf3e9c8214b4aa4a2b1529
Andy Allan <Andy4202@gmail.com>
2022-04-30 17:14:08 +0100

    Challenge 3 complete.

--- server.js
+++ server.js
@@ -8,19 +8,22 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const pug = require('pug');
 const app = express();
 
-app.set('view engine', 'pug');
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(passport.initialize());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: {secure:false}
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'pug');
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 
 app.route('/').get((req, res) => {
